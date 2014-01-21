26296624838d28a65c69d8184d3d49258769b2d6
romainhk <romainhk@gmail.com>
2014-01-21 21:51:52 +0100

    compilation simple pour windows x64

--- compile.bat
+++ compile.bat
@@ -1,14 +1,14 @@
 rem == Compilation du programme ==
-rem Necessite : Python for windows 3.3, cx_freeze et 7zip SFX M
+rem Necessite : Python for windows 3.3, cx_freeze
 set build=build\exe.win-amd64-3.3
 rem rd /S /Q %build%
 C:\Python33\python.exe setup.py build
 
-set output=legion
+rem set output=legion
 rem rd /S /Q %output%
 rem mkdir %output%
-
 rem xcopy /Y %build%\* %output%
-rem cp base.sqlite %output%
 
-rem 7zip sfx
+copy base.sqlite %build%
+
+rem Le dossier "%build%" contient maintenant de quoi executer Legion sur windows

734a4d6b435f5f60c60202b54f24cdd99b1e684f
romainhk <romainhk@gmail.com>
2014-02-01 15:11:15 +0100

    input à la demande uniquement sur la colonne "Après"

--- web/scripts.js
+++ web/scripts.js
@@ -25,25 +25,44 @@ function envoie_du_fichier(event) {
     $.post('/importation', { data: result, name: fileName }, function(reponse) {
         $.jGrowl(reponse, { header: 'Importation', life : 6000 });
         $("#progress").hide();
-        charger_page('Liste');
+        $( "#onglets li:first-child" ).trigger( "click" );
         stats_annees(); // Une nouvelle année est peut-être disponible...
     });
 }
 
 /*
- * Mets à jour un champ de la base
+ * Système d'input pour modifier le contenu d'une colonne
  */
-function maj_champ(ine, champ) {
-    input = $("#"+champ[0]+"--"+ine);
-    if (input.val() != '?') {
-        params = "ine="+ine+"&champ="+champ+"&d="+input.val();
-        $.get( "/maj?"+params, function( data ) {
-            if (data == 'Oui') { input.addClass("maj_oui"); }
-            else if (data == 'Non') { input.addClass("maj_non"); }
-        });
+/*   Création de l'input   */
+function add_input() {
+    var td = $(this);
+    td.removeClass('maj_oui maj_non'); // Nettoyage du marqueur d'envoie précédent
+    if ( td.children().first().prop("tagName") != "INPUT" ) { // S'il n'y a pas encore d'input
+        var val = td.html();
+        if (val == "?") { val = ""; }
+        td.html("<input type='text' value='"+val+"' size='12'></input>");
+        td.children().first().focus(); // Focus auto sur ce nouvel input
     }
 }
-function maj_raz(id) { $("#"+id).removeClass('maj_oui maj_non'); }
+/*   Envoie de la modification   */
+function push_input() {
+    var td = $(this);
+    if ( td.html() != "?" ) {
+        var input = td.children().first();
+        var val = input.val();
+        old = input.attr('value');
+        if (val != "" && val != old) {
+            var ine = td.parent().attr('id');
+            var champ = "Après";
+            params = "ine="+ine+"&champ="+champ+"&d="+val;
+            $.get( "/maj?"+params, function( data ) {
+                if (data == 'Oui') { td.addClass("maj_oui"); }
+                else if (data == 'Non') { td.addClass("maj_non"); }
+            });
+        }
+        td.html(val); // Suppression de l'input
+    }
+}
 
 /* 
  * Convertir une liste en lignes de tableau (tr)
@@ -59,25 +78,30 @@ function list_to_tab(liste, champs) {
                 v = v.replace(/([^, ]+)\*/g, '<span class="doublement">$1</span>');
             } else if (j == "Genre") { // Traduction de la colonne genre
                 if (v == "1") { v = "Homme"; } else if (v == "2") { v = "Femme"; }
-            } else if (j == "Diplômé" || j == "Après") { // Ajout d'input
-                id = j[0] + '--' + ine;
-                v = "<input id='"+id+"' type='text' value='"+v+"' onblur=\"maj_champ('"+ine+"', '"+j+"');\" onfocus=\"maj_raz('"+id+"');\" size='10'></input>";
             }
             vals += "<td>"+v+"</td>";
         });
-        lignes += "<tr>"+vals+"</tr>\n";
+        lignes += "<tr id='"+ine+"'>"+vals+"</tr>\n";
     });
     return(lignes);
 }
 
-/* Le switch de page
+/* 
+ * Le switch de page
  */
 function charger_page(nom) {
     $.each(les_pages, function( i, p ) { $("#"+p).hide(); });
     if (nom == 'Liste') {
         page_active = 'Liste';
         $.get( "/liste", function( data ) {
+            // Construction du tableau
             $('#vue > tbody').html( list_to_tab(data, champs_vue) );
+            // Ajout des input auto sur la colonne 'Après'
+            index = $.inArray('Après', champs_vue);
+            var col_apres = $('#vue > tbody td:nth-child('+(index+1)+')');
+            col_apres.on('click', add_input);
+            col_apres.on('focusout', push_input);
+
             $.jGrowl("Chargement des "+data.length+" élèves terminé.", { life : 3000 });
             $("#vue").tablesorter({
                 theme:'blue',

73a1c7c2e7dfdbd627da9916b3ebaff62f94b8af
romainhk <romainhk@gmail.com>
2014-05-06 21:03:04 +0200

    compilation en 3.4

--- compile.bat
+++ compile.bat
@@ -1,8 +1,8 @@
 rem == Compilation du programme ==
-rem Necessite : Python for windows 3.3, cx_freeze, 7-zip
-set build=build\exe.win-amd64-3.3
+rem Necessite : Python for windows 3.4, cx_freeze, 7-zip
+set build=build\exe.win-amd64-3.4
 rem rd /S /Q %build%
-C:\Python33\python.exe setup.py build
+C:\Python34\python.exe setup.py build
 
 copy base.sqlite %build%
 
